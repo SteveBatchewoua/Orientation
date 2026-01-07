c393a2ec867a636b91254786ec60036611060506
Steve Batchewoua <batchewoua2006@yahoo.Fr>
2026-01-07 23:20:55 +0100

    feat: ajouter injectSpeedInsights()

--- estm-web/src/main.ts
+++ estm-web/src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient()],
 });
+injectSpeedInsights();
